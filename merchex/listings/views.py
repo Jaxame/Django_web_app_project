2536e797929773998222908e19f2d6939f82f7c4
JACKY AME <jacky.ame@orange.fr>
2022-10-27 14:33:00 +0200

    Update views.py and urls.py after the update band_list.html file

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -2,11 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from listings.models import Band, Listing
 
-def hello(request):
-    bands = Band.objects.all()
-    return render(request,
-                 'listings/hello.html',
-                 {'bands': bands})
+def band_list(request):  # renommer la fonction de vue
+   bands = Band.objects.all()
+   return render(request,
+           'listings/band_list.html',  # pointe vers le nouveau nom de modèle
+           {'bands': bands})
 
 def about(request):
     return render(request,
